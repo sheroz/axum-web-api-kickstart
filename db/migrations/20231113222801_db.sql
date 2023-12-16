f1fbf1b5c0b3e9fc519ee1b7d976c68718dd08fc
Sheroz <25744927+sheroz@users.noreply.github.com>
2023-12-16 06:08:22 +0300

    added sqlx support to user model (#16)

--- db/migrations/20231113222801_db.sql
+++ db/migrations/20231113222801_db.sql
@@ -1,25 +1,27 @@
-CREATE TABLE users (
-    id uuid primary key default gen_random_uuid(),
-    username text not null,
-    email text unique not null,
-    pswd_hash text not null,
-    pswd_salt text not null,
+CREATE TABLE users
+(
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username    TEXT        NOT NULL,
+    email       TEXT UNIQUE NOT NULL,
+    pswd_hash   TEXT        NOT NULL,
+    pswd_salt   TEXT        NOT NULL,
     -- limit access rate
-    last_access timestamp
+    last_access TIMESTAMP,
+    created_at  TIMESTAMP   NOT NULL,
+    updated_at  TIMESTAMP   NOT NULL,
+    deleted_at  TIMESTAMP
 );
 
-INSERT INTO users (
-        username,
-        email,
-        pswd_hash,
-        pswd_salt,
-        last_access
-    )
-VALUES (
-        'admin',
+INSERT INTO users (username,
+                   email,
+                   pswd_hash,
+                   pswd_salt,
+                   created_at,
+                   updated_at)
+VALUES ('admin',
         'admin@admin.com',
-        -- password: pswd1234, hash(pswd1234pjZKk6A8YtC8$9p&UIp62bv4PLwD7@dF)
+           -- password: pswd1234, hash(pswd1234pjZKk6A8YtC8$9p&UIp62bv4PLwD7@dF)
         '7c44575b741f02d49c3e988ba7aa95a8fb6d90c0ef63a97236fa54bfcfbd9d51',
         'pjZKk6A8YtC8$9p&UIp62bv4PLwD7@dF',
-        now()
-    );
+        now(),
+        now());
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod auth;
 pub mod config;
 pub mod state;
-pub mod user;
+pub mod users;
